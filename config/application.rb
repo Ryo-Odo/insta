f5abf09869bcb288b5a37ebaa193043d619fb423
Ryo_Odo <marukae@gmail.com>
2021-11-17 07:58:14 +0000

    ログイン機能追加

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,14 @@ Bundler.require(*Rails.groups)
 
 module InstaApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.load_defaults 6.0
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
